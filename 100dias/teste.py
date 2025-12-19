c320cd6639467400a3dd8e079bb5ee3616702c21
carolinab-png <carolinabarrosadvocacia@gmail.com>
2025-12-19 15:53:42 -0300

    Delete 100dias directory

--- 100dias/teste.py
+++
@@ -1,7 +0,0 @@
-pi = 3.14159265
-formatted_num = "{:.2f}".format(pi)
-print(f"O valor de PI arredondado é", str(formatted_num))
-
-rounded_num = round(pi, 3)
-print(f"O valor de PI arredondado é", str(rounded_num))
-print(f'O valor de pi com 2 casas decimais é {pi:.2f}')
